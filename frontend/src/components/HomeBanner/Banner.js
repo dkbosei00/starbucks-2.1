1ac4c195a063a89743ad750a096144042dfab717
Andy-Aseye <andyaseye@gmail.com>
2023-08-11 11:36:52 +0000

    implementation of responsiveness in banner component

--- frontend/src/components/HomeBanner/Banner.js
+++ frontend/src/components/HomeBanner/Banner.js
@@ -3,8 +3,7 @@ import styles from "./styles.module.css";
 import Starbucks from "../../assets/starbucks-restaurant.jpg";
 import Boy from "../../assets/boy-smiling.jpg";
 import Workers from "../../assets/workers.jpg";
-import {FaQuoteLeft, FaQuoteRight} from "react-icons/fa";
-
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 
 const Banner = () => {
   return (
@@ -16,18 +15,20 @@ const Banner = () => {
               Stop by a <br /> shop today
             </h1>
             <p>
-              From housemade sausages to racks of ribs, we love to feed our
-              Kansas City community. So come join the party… and come hungry!
+              From handcrafted beverages to delightful pastries, we love to
+              serve our Starbucks community. So come join the experience... and
+              come ready to indulge!
             </p>
             <p>
               <i>
-                PLEASE KNOW JONES BAR-B-Q RESTAURANT IS STILL OPEN WITH THE
-                SHORTY AND LITTLE SISTERS RUNNING IT! WE WOULD LOVE TO SEE YOU.
+                PLEASE NOTE THAT STARBUCKS COFFEEHOUSE IS STILL OPEN WITH YOUR
+                FAVORITE BARISTAS CREATING YOUR SPECIAL DRINKS! WE CAN'T WAIT TO
+                SERVE YOU.
               </i>
             </p>
             <p>
-              Starting in October 2022 we will have a new Hot BBQ sauce to check
-              out.
+              Starting in October 2023, we will introduce a new signature drink
+              to explore.
             </p>
 
             <button className={styles.button_49}>20% OFF</button>
@@ -41,7 +42,9 @@ const Banner = () => {
         <div className={styles.banner2_inner2}>
           <div className={styles.banner2_main}>
             <h1>
-              <FaQuoteLeft size={52} style={{marginTop: "-8vh"}}/>Everything I brew, <br />I brew it for you<FaQuoteRight size={52} style={{marginTop: "-8vh"}}/>
+              <FaQuoteLeft className={styles.quote_left}/>
+              Everything I brew, <br />I brew it for you
+              <FaQuoteRight className={styles.quote_right}/>
             </h1>
             <p>- STARBUCKS</p>
           </div>
@@ -52,10 +55,11 @@ const Banner = () => {
           <img src={Boy} alt="" className={styles.boy_img} />
           <div className={styles.banner3_inner_btm}>
             <img src={Workers} alt="" className={styles.worker_img} />
-            <p className={styles.banner3_quote}> 
-              Beneath the mermaid's gaze, we meet, <br/> In Starbucks' haven, cozy
-              seat. <br/>A latte swirls, a mocha dream,<br/> Sipping stories, a silent
-              stream.<br/> Caffeine's embrace, a poet's retreat.
+            <p className={styles.banner3_quote}>
+              Beneath the mermaid's gaze, we meet, <br /> In Starbucks' haven,
+              cozy seat. <br />A latte swirls, a mocha dream,
+              <br /> Sipping stories, a silent stream.
+              <br /> Caffeine's embrace, a poet's retreat.
             </p>
           </div>
         </div>
