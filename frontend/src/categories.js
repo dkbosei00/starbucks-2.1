c5135afc414012ad15d0b43c4a6128536ef202a0
Andy-Aseye <andyaseye@gmail.com>
2023-09-16 17:30:24 +0000

    modifications to categories component

--- frontend/src/categories.js
+++ frontend/src/categories.js
@@ -1,7 +1,10 @@
-import Drinks from './assets/drinks.png';
-import Food from './assets/food.png';
-import Homemade from './assets/homemade.png';
-import Merch from './assets/merch.png';
+// import Drinks from './assets/drinks.png';
+// import Food from './assets/food.png';
+// import Homemade from './assets/homemade.png';
+import Homemade from './assets/beans-background.jpg';
+import Drinks from './assets/starbucks-drink.png';
+import Food from './assets/starbucks-pastry.png';
+import Merch from './assets/starbucks-merch-bg.png';
 
 
 const categories = [
