5bf54f20d5a5f4f59fb085e5d204f8a757098457
Daniel <dannykosei00@gmail.com>
2024-03-11 22:36:21 +0000

    Connected MongoDB using Go; Tried different file func import

---
+++ go-backend/connection.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connect() {
+	// Find .env
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Error loading env file: %s", err)
+	}
+
+	// Get value
+	db := os.Getenv("DB")
+
+	// Connect to the database
+	clientOptions := options.Client().ApplyURI(db)
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Disconnect db when func stops (clean up)
+	// defer func() {
+	// 	if err := client.Disconnect(context.TODO()); err != nil {
+	// 		panic(err)
+	// 	}
+	// }()
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connected to db!")
+	}
+}
